a2be49cebd7d64ddc9bc2510a1bea7bb6bc7adb1
Maverick <maver_ck@outlook.com>
2019-02-23 04:51:50 +0800

    global kicks notifications
    Signed-off-by: Yasir-siddiqui <www.mohammad.yasir.s@gmail.com>

--- tg_bot/modules/global_kick.py
+++ tg_bot/modules/global_kick.py
@@ -51,15 +51,24 @@ def gkick(bot: Bot, update: Update, args: List[str]):
     if int(user_id) == OWNER_ID:
         message.reply_text("Wow! Someone's so noob that he want to gkick my owner! *Grabs Potato Chips*")
         return
-    if int(user_id) == bot.id:
-        message.reply_text("OHH! Let's do it! Let me kick myself! And then loose all my users! -_-")
-        return
+        
+    if user_id == bot.id:
+        message.reply_text("Welp, I'm not gonna to gkick myself!")
+        return    
+
     chats = get_all_chats()
-    message.reply_text("Globally kicking user @{}".format(user_chat.username))
     banner = update.effective_user  # type: Optional[User]
     send_to_list(bot, SUDO_USERS + SUPPORT_USERS,
-                   "{} has been successfully gkicked!".format(mention_html(user_chat.id, user_chat.first_name)),
-                   html=True)  
+                 "<b>Global Kick</b>" \
+                 "\n#GKICK" \
+                 "\n<b>Status:</b> <code>Enforcing</code>" \
+                 "\n<b>Sudo Admin:</b> {}" \
+                 "\n<b>User:</b> {}" \
+                 "\n<b>ID:</b> <code>{}</code>".format(mention_html(banner.id, banner.first_name),
+                                              mention_html(user_chat.id, user_chat.first_name), 
+                                                           user_chat.id), 
+                html=True)
+    message.reply_text("Globally kicking user @{}".format(user_chat.username))
     for chat in chats:
         try:
              bot.unban_chat_member(chat.chat_id, user_id)  # Unban_member = kick (and not ban)
@@ -71,7 +80,40 @@ def gkick(bot: Bot, update: Update, args: List[str]):
                 return
         except TelegramError:
             pass
+        
+@run_async
+@user_admin
+def gkickstat(bot: Bot, update: Update, args: List[str]):
+    if len(args) > 0:
+        if args[0].lower() in ["on", "yes"]:
+            sql.enable_gkick(update.effective_chat.id)
+            update.effective_message.reply_text("I've enabled gkicks in this group. This will help protect you "
+                                                "from spammers and unsavoury characters.")
+        elif args[0].lower() in ["off", "no"]:
+            sql.disable_gkick(update.effective_chat.id)
+            update.effective_message.reply_text("I've disabled gkicks in this group. GKicks wont affect your users "
+                                                "anymore. You'll be less protected from spammers though!")
+    else:
+        update.effective_message.reply_text("Give me some arguments to choose a setting! on/off, yes/no!\n\n"
+                                            "Your current setting is: {}\n"
+                                            "When True, any gkicks that happen will also happen in your group. "
+                                            "When False, they won't, leaving you at the possible mercy of "
+                                            "spammers.".format(sql.does_chat_gkick(update.effective_chat.id)))
+
+__help__ = """
+*Admin only:*
+ - /gkickstat <on/off/yes/no>: Will disable the effect of global kicks on your group, or return your current settings.
+Gkick, also known as global kicks, are used by the bot owners to kick spammers across all groups. This helps protect \
+you and your groups by removing spam flooders as quickly as possible. They can be disabled for you group by calling \
+/gkickstat
+"""
+
+__mod_name__ = "Global Kicks"
 
 GKICK_HANDLER = CommandHandler("gkick", gkick, pass_args=True,
                               filters=CustomFilters.sudo_filter | CustomFilters.support_filter)
-dispatcher.add_handler(GKICK_HANDLER)                              
+
+GKICK_STATUS = CommandHandler("gkickstat", gkickstat, pass_args=True, filters=Filters.group)
+
+dispatcher.add_handler(GKICK_HANDLER)
+dispatcher.add_handler(GKICK_STATUS)
