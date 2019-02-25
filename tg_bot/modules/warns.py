c47498307e34662675b4be1f94a7293d5b7e2850
Yasir-siddiqui <www.mohammad.yasir.s@gmail.com>
2019-02-25 10:22:09 +0000

    Warn: fix a typo!
    Signed-off-by: Yasir-siddiqui <www.mohammad.yasir.s@gmail.com>

--- tg_bot/modules/warns.py
+++ tg_bot/modules/warns.py
@@ -66,10 +66,10 @@ def warn(user: User, chat: Chat, reason: str, message: Message, warner: User = N
         keyboard = InlineKeyboardMarkup(
             [[InlineKeyboardButton("Remove warn (admin only)", callback_data="rm_warn({})".format(user.id))]])
 
-        reply = "{} <b>has been WARNED!</b> \n Count: {}/{}".format(mention_html(user.id, user.first_name), num_warns,
+        reply = "{} <b>has been WARNED!</b> \nCount:{}/{}".format(mention_html(user.id, user.first_name), num_warns,
                                                              limit)
         if reason:
-            reply += "\nReason for last warn:\n{}".format(html.escape(reason))
+            reply += "\nReason: {}".format(html.escape(reason))
 
         log_reason = "<b>{}:</b>" \
                      "\n#WARN" \
